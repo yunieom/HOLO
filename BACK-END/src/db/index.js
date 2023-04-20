4d34313cee627303b370d340d6f82824f94e8fb0
H0onnn <116232939+H0onnn@users.noreply.github.com>
2023-04-20 14:35:12 +0900

    회원가입 API 구현

---
+++ BACK-END/src/db/index.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const { MONGODB_URL } = process.env;
+const mongoose = require('mongoose');
+
+
+mongoose.connect(MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+
+db.on('connected', () => {
+    console.log('MongoDB 서버에 연결되었습니다. ' + MONGODB_URL);
+});
+db.on('error', err => {
+    console.error('\nMongoDB 연결에 실패하였습니다...\n' + MONGODB_URL + '\n' + err);
+});
+
+module.exports = db;
