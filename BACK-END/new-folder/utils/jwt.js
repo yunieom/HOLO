90e379ec839fedc9537f3065ae64cefafdf80471
paikpaik <woduf9311@gmail.com>
2023-04-20 22:26:00 +0900

    폴더 동기화를 위한 폴더 삭제

--- BACK-END/new-folder/utils/jwt.js
+++
@@ -1,8 +0,0 @@
-const jwt = require('jsonwebtoken'); // jwt 모듈 불러오기
-
-const generateToken = (payload) => {
-    const token = jwt.sign(payload, process.env.JWT_SECRET_KEY, { expiresIn: '1h' });
-    return token;
-}; // jwt.sign() 메서드를 통해 jwt 토큰 발행. expiresIn : '1h' 설정으로 1시간 후 토큰이 만료되게 설정.
-
-module.exports = generateToken;
