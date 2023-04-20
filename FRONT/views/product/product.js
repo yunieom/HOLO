bee84ae9f05cf07816f135d33372746acac201ad
solssak <gswoong1012@gmail.com>
2023-04-20 14:33:17 +0900

    init: product layout

---
+++ FRONT/views/product/product.js
@@ -0,0 +1,7 @@
+const productPrice = () => {};
+const productName = () => {};
+const discountRate = () => {};
+const shortDesc = () => {};
+const longDesc = () => {};
+const imageUrl = () => {};
+const stock = () => {};
