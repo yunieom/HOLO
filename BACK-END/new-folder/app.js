6b8e197d209974d454d0696ec08e43fcfec9f747
paikpaik <woduf9311@gmail.com>
2023-04-20 22:16:07 +0900

    폴더구조를 통일시키기 위한 커밋

---
+++ BACK-END/new-folder/app.js
@@ -0,0 +1,15 @@
+const express = require('express'); // express 불러오기
+const app = express(); // express 실행
+const userRouter = require('./routers/userRouter'); // user 라우터 불러오기
+
+// JSON 형식의 데이터를 파싱하기 위한 미들웨어
+app.use(express.json());
+
+// URL-encoded 형식의 데이터를 파싱하기 위한 미들웨어
+app.use(express.urlencoded({ extended: false }));
+
+// user 라우터 사용
+app.use('/api/users', userRouter);
+
+// app 객체 모듈로 내보내기
+module.exports = app;
