163dc5f3c12500c0b182088a1200ae55941db886
solssak <gswoong1012@gmail.com>
2023-04-26 20:55:37 +0900

    feat: 장바구니 상품 전체 선택, 장바구니 상품 보여주기

--- FRONT/views/shoppingCart/shoppingCart.js
+++ FRONT/views/shoppingCart/shoppingCart.js
@@ -2,36 +2,81 @@ const cartItem = document.querySelector(".cart-item");
 const purchaseBtn = document.querySelector(".purchase-btn");
 const productPrice = document.querySelector(".product-price");
 const productQuantity = document.querySelector(".quantity");
+const cartContainer = document.querySelector(".cart-container");
+const checkAll = document.getElementById("allCheck");
+const totalPriceText = document.querySelector(".total-price");
+const decreaseBtn = document.getElementById("decrease-btn");
+const increaseBtn = document.getElementById("increase-btn");
 
 const menu = JSON.parse(localStorage.getItem("menu"));
-console.log(typeof menu);
 
-const purchaseBtnHandler = () => {
-  const { productName, price } = menu.orderItems;
-  const { quantity } = menu.orderItems;
+let totalPrice = 0;
 
-  cartItem.innerText = productName;
-  productPrice.innerText = price;
-  productQuantity.innerText = quantity;
+const purchaseBtnHandler = (item) => {
+  const { productName, price, quantity } = item.orderItems;
+  totalPrice += Number(price) * quantity;
+
+  const paintProduct = ` <div class="container text-left" >
+<div class="row" >
+  <div class="col">
+    <div class="form-check">
+      <input class="form-check-input painted" type="checkbox" value="" id="defaultCheck1">
+      <label class="form-check-label" for="defaultCheck1">
+        <p class="cart-item">${productName}</p>
+      </label>
+    </div>
+  </div>
+
+  <div class="col">
+  <div id="itemCount">
+  <button
+  id="decrease-btn"
+  type="button"
+  class="btn btn-secondary btn-sm"
+  style="height: 1.5rem"
+  >
+  -
+  </button>
+  <p class="quantity">${quantity}</p>
+  <button
+  id="increase-btn"
+  type="button"
+  class="btn btn-secondary btn-sm"
+  style="height: 1.5rem"
+  >
+  +
+  </button>
+  </div>
+  </div>
+
+  <div class="col">
+    <p class="product-price">${price}원</p>
+  </div>
+  <div class="col">
+    <div>3,000원</div>
+  </div>
+</div>
+</div>`;
+  cartContainer.insertAdjacentHTML("beforeend", paintProduct);
 };
 
-//
-// async function getItem() {}
+menu.forEach(purchaseBtnHandler);
 
-menu.forEach(
-  (item) => console.log(item)
-  // const { productName, price, quantity } = orderItems;
-  // cartItem.innerText = productName;
-  // productPrice.innerText = price;
-  // productQuantity.innerText = quantity;
-);
+totalPriceText.innerText = totalPrice;
 
-//   // const { productName, price } = menu.orderItems;
-//   // const { amount } = menu;
+const checkBoxs = document.querySelectorAll(".form-check-input.painted");
 
-//   // cartItem.innerText = productName;
-//   // productPrice.innerText = price;
-//   // productQuantity.innerText = amount;
-// };
+const checkAllHandler = (e) => {
+  if (e.target.checked) {
+    checkBoxs.forEach((checkbox) => {
+      checkbox.checked = true;
+    });
+  } else {
+    checkBoxs.forEach((checkbox) => {
+      checkbox.checked = false;
+    });
+  }
+};
 
 purchaseBtn.addEventListener("click", purchaseBtnHandler);
+checkAll.addEventListener("change", checkAllHandler);
