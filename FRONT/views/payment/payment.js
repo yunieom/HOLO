77fb54de63f61ec56d08e8890154486fa0c1edab
puranium235 <puranium235@gmail.com>
2023-04-27 03:03:42 +0900

    feat: 비로그인시 구매자정보 안보이게

--- FRONT/views/payment/payment.js
+++ FRONT/views/payment/payment.js
@@ -1,16 +1,16 @@
 import * as Api from "../api.js";
 
-const userName = document.querySelector("#name");
-const userEmail = document.querySelector("#email");
-const userPhoneNumber = document.querySelector("#phoneNumber");
-const userAddress = document.querySelector("#userAddress");
+// const userName = document.querySelector("#name");
+// const userEmail = document.querySelector("#email");
+// const userPhoneNumber = document.querySelector("#phoneNumber");
+// const userAddress = document.querySelector("#userAddress");
 const infoBtn = document.querySelector("#infoBtn");
 const receiverName = document.querySelector("#receiverName");
 const receiverEmail = document.querySelector("#receiverEmail");
 const receiverPhoneNumber = document.querySelector("#receiverPhoneNumber");
-const address = document.querySelector("#address");
+const userAddress = document.querySelector("#address");
 const receiverRequirement = document.querySelector("#receiverRequirement");
-
+const isLogin = sessionStorage.getItem('token');
 
 // user-info에서 get 받아오기
 async function getData() {
@@ -20,22 +20,39 @@ async function getData() {
         alert(err.message);
     }
 }
-// 유저정보 넣어주기
-async function loadUser(){
+// 로그인시에는 구매자 정보 띄워줌
+if (isLogin) {
     const {name, email, phoneNumber, address} = await getData();
-    userName.innerText = name;
-    userEmail.innerText = email;
-    userPhoneNumber.innerText = phoneNumber;
-    userAddress.innerText = address;
-}
-await loadUser();
+    const userInfo = `
+    <h2 class="fs-5">구매자 정보</h2>
+            <section class="mb-3 border border-dark px-4 py-3">
+                <div class="row mb-2">
+                    <div class="col-3 fs-6">이름</div>
+                    <div class="col" id="name">${name}</div>
+                </div>
+                <div class="row mb-2">
+                    <div class="col-3 fs-6">이메일</div>
+                    <div class="col" id="email">${email}</div>
+                </div>
+                <div class="row mb-2">
+                    <div class="col-3 fs-6">연락처</div>
+                    <div class="col" id="phoneNumber">${phoneNumber}</div>
+                </div>
+                <div class="row">
+                    <div class="col-3 fs-6">주소</div>
+                    <div class="col" id="userAddress">${address}</div>
+                </div>
+            </section>
+    `
+    document.querySelector("main").insertAdjacentHTML("afterbegin", userInfo);
+    infoBtn.addEventListener("click", (e) => {
+        e.preventDefault();
+        receiverName.value = name;
+        receiverEmail.value = email;
+        receiverPhoneNumber.value = phoneNumber;
+        userAddress.value = address;
+    });
+} else {
+    infoBtn.style.display = 'none';
 
-function handleUserInfo(e){
-    e.preventDefault();
-    receiverName.value = userName.innerText;
-    receiverEmail.value = userEmail.innerText;
-    receiverPhoneNumber.value = userPhoneNumber.innerText;
-    address.value = userAddress.innerText;
 }
-
-infoBtn.addEventListener("click", handleUserInfo);
