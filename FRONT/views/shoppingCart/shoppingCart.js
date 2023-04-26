bf4a9d16232aa0ef201796a5005eb0fdbedaee88
solssak <gswoong1012@gmail.com>
2023-04-26 15:50:59 +0900

    fix: git 오류로 인한 파일 복구

---
+++ FRONT/views/shoppingCart/shoppingCart.js
@@ -0,0 +1,38 @@
+const cartItem = document.querySelector(".cart-item");
+const purchaseBtn = document.querySelector(".purchase-btn");
+const productPrice = document.querySelector(".product-price");
+const productQuantity = document.querySelector(".quantity");
+
+const menu = JSON.parse(localStorage.getItem("menu"));
+console.log(typeof menu);
+
+// const purchaseBtnHandler = () => {
+//   const { productName, price } = menu.orderItems;
+//   const { quantity } = menu.orderItems;
+
+//   cartItem.innerText = productName;
+//   productPrice.innerText = price;
+//   productQuantity.innerText = quantity;
+// };
+
+//
+// async function getItem() {}
+
+menu.forEach(
+  (item) => console.log(item)
+  // const { productName, price, quantity } = orderItems;
+  // cartItem.innerText = productName;
+  // productPrice.innerText = price;
+  // productQuantity.innerText = quantity;
+);
+const purchaseBtnHandler = () => {};
+
+//   // const { productName, price } = menu.orderItems;
+//   // const { amount } = menu;
+
+//   // cartItem.innerText = productName;
+//   // productPrice.innerText = price;
+//   // productQuantity.innerText = amount;
+// };
+
+purchaseBtn.addEventListener("click", purchaseBtnHandler);
