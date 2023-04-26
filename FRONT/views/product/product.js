61d8582f0f8f1cca3c736ced96f2e8f57a8dfb4d
solssak <gswoong1012@gmail.com>
2023-04-26 14:55:47 +0900

    fix: git 오류로 인한 파일 복구

--- FRONT/views/product/product.js
+++ FRONT/views/product/product.js
@@ -1,136 +1,168 @@
-import * as API from "../api.js";
+// import * as API from "/api/apiTemplate.js";
 
 const addCartItemBtn = document.getElementById("add-cart-item-btn");
 const inquiriesBtn = document.getElementById("inquiries");
-const amount = document.getElementById("amount");
+const quantity = document.getElementById("quantity");
 const decreaseBtn = document.getElementById("decrease-btn");
 const increaseBtn = document.getElementById("increase-btn");
 const mainContent = document.querySelector(".main-content");
 const infoBtn = document.getElementById("info");
 
-// async function data() {
-//   return await API.get("/api/products/product");
-// }
+const productName = document.querySelector(".product-name");
+const price = document.querySelector(".price");
 
-// async function createInquiries() {
-//   return await API.post("/api/products/inquiries");
-// }
+let productQuantity = 1;
 
-let menu = [
-  {
-    product: { productName: "메뉴이름1", price: "19,000" },
-    amount: Number(amount.textContent),
-  },
-  {
-    product: { productName: "메뉴이름2", price: "32,000" },
-    amount: Number(amount.textContent),
-  },
-  {
-    product: { productName: "메뉴이름3", price: "21,000" },
-    amount: Number(amount.textContent),
-  },
-];
+async function getData() {
+  const res = await fetch(`/api/product//1`, {
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
+  });
+
+  const data = await res.json();
+  console.log(data);
+  productName.innerText = data.productName;
+  price.innerText = data.price;
+
+  return data;
+}
+
+const data = await getData();
+console.log(data);
+
+// setStorage
+const addCartItemBtnHandler = () => {
+  if (menu) {
+    const idx = menu.findIndex(
+      (m) => m.orderItems.productName === data.productName
+    );
+    if (idx != -1) {
+      // 이미 장바구니에 상품이 있을 때
+      menu[idx].orderItems.quantity += productQuantity;
+    } else {
+      // 장바구니에 없는 새로운 상품일 때
+      addNewProduct(menu);
+    }
+  } else {
+    // 장바구니에 아무런 상품도 없을 때
+    menu = [];
+    addNewProduct(menu);
+  }
+  window.localStorage.setItem("menu", JSON.stringify(menu));
+};
+
+const addNewProduct = (menu) => {
+  let newProduct = {
+    orderItems: {
+      productName: data.productName,
+      price: data.price,
+      quantity: productQuantity,
+    },
+  };
+  menu.push(newProduct);
+};
+
+async function createInquiries() {
+  return await API.post("/api/products/inquiries");
+}
+
+let menu = JSON.parse(localStorage.getItem("menu"));
 
 // 상품 수량 조절
 const decreaseBtnHandler = () => {
-  if (amount.innerText > 1) {
-    amount.innerText = Number(amount.innerText) - 1;
-    menu.amount -= 1;
+  if (quantity.innerText > 1) {
+    quantity.innerText = Number(quantity.innerText) - 1;
+    productQuantity -= 1;
   }
 };
 
 const increaseBtnHandler = () => {
-  amount.innerText = Number(amount.innerText) + 1;
-  menu.amount += 1;
-};
-
-// setStorage
-const addCartItemBtnHandler = () => {
-  window.localStorage.setItem("menu", JSON.stringify(menu));
+  quantity.innerText = Number(quantity.innerText) + 1;
+  productQuantity += 1;
 };
 
 // 상품 상세페이지 HTML
 const detailInfo = ` 
 <div class="row">
-  <img src="..." class="rounded float-start" alt="이미지" />
+<img src="..." class="rounded float-start" alt="이미지" />
 </div>`;
 
 // 상품 문의 HTML
 const inquiriesList = `
-  <div class="row">
-    <div class="question-box">
-      <table class="table table-sm table-hover">
-        <thead class="table-light">
-          <tr>
-            <th scope="col">순서</th>
-            <th scope="col">제목</th>
-            <th scope="col">작성자</th>
-            <th scope="col">작성일</th>
-            <th scope="col">답변상태</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>진짜 맛있나요? 진짜 맛있나요? 진짜 맛있나요?</td>
-            <td>퓨라늄</td>
-            <td>23.04.18</td>
-            <td>답변완료</td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>생으로 먹어도 맛있네요</td>
-            <td>김소현</td>
-            <td>23.04.19</td>
-            <td>미응답</td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td>사이즈가 딱 맞네요!</td>
-            <td>최현준</td>
-            <td>23.04.19</td>
-            <td>미응답</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-    <div class="container edit-btn">
-    <div class="row-12">
-      <button id="inquiriesList-btn" type="button" class="btn btn-success">
-        글쓰기
-      </button>
-    </div>
-  </div>
+<div class="row">
+<div class="question-box">
+<table class="table table-sm table-hover">
+<thead class="table-light">
+<tr>
+<th scope="col">순서</th>
+<th scope="col">제목</th>
+<th scope="col">작성자</th>
+<th scope="col">작성일</th>
+<th scope="col">답변상태</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+<th scope="row">1</th>
+<td>진짜 맛있나요? 진짜 맛있나요? 진짜 맛있나요?</td>
+<td>퓨라늄</td>
+<td>23.04.18</td>
+<td>답변완료</td>
+</tr>
+<tr>
+<th scope="row">2</th>
+<td>생으로 먹어도 맛있네요</td>
+<td>김소현</td>
+<td>23.04.19</td>
+<td>미응답</td>
+</tr>
+<tr>
+<th scope="row">3</th>
+<td>사이즈가 딱 맞네요!</td>
+<td>최현준</td>
+<td>23.04.19</td>
+<td>미응답</td>
+</tr>
+</tbody>
+</table>
+</div>
+<div class="container edit-btn">
+<div class="row-12">
+<button id="inquiriesList-btn" type="button" class="btn btn-success">
+글쓰기
+</button>
+</div>
+</div>
 </div>`;
 
 // 상품 문의 작성 HTML
 const inquiriesbox = `
 <div class="container">
 <div class="row">
-  <div class="list-box">
-    <ul id="list">
-      <li>
-        <p class="content">{게시글 본문 내용}</p>
-      </li>
-    </ul>
-  </div>
+<div class="list-box">
+<ul id="list">
+<li>
+<p class="content">{게시글 본문 내용}</p>
+</li>
+</ul>
+</div>
 </div>
 </div>
 <div class="container write-input">
 <div class="row">
-  <div>
-    <label for="userName">작성자</label>
-    <input type="text" id="userName" />
-  </div>
-  <div>
-    <label for="title">제목</label>
-    <input type="text" id="title" />
-  </div>
-  <div>
-    <label for="content">내용</label>
-    <textarea id="content"></textarea>
-  </div>
-  <button id="btnAddTxt" class="btn btn-success">글쓰기</button>
+<div>
+<label for="userName">작성자</label>
+<input type="text" id="userName" />
+</div>
+<div>
+<label for="title">제목</label>
+<input type="text" id="title" />
+</div>
+<div>
+<label for="content">내용</label>
+<textarea id="content"></textarea>
+</div>
+<button id="btnAddTxt" class="btn btn-success">글쓰기</button>
 </div>
 </div>`;
 
