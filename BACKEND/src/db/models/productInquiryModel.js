38e258cc58ac819aef469362b873657f2f04b227
paikpaik <woduf9311@gmail.com>
2023-04-27 18:41:26 +0900

    상품문의관련 파일 제거

--- BACKEND/src/db/models/productInquiryModel.js
+++
@@ -1,5 +0,0 @@
-// const mongoose = require('mongoose');
-// const ProductInquirySchema = require('../schemas/productInquiry');
-
-// //Category 모델 생성
-// exports.ProductInquiry = mongoose.model('ProductInquiry', ProductInquirySchema);
